523d09f2ab1f5c7e82572b27f3052f79c3bac64d
Josip Celic <josipcelic22@gmail.com>
2022-12-02 20:54:13 +0100

    js-domaci

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 /*
 ZADATAK 1
-Napisi program koji ovisno o varijabli time mogucih vrijednosti ZORA, JUTRO, POSLIJEPODNE, POPODNE, PRIJEPODNE, PREDVECER, NOC ispisuje Dobro jutro, Dobar dan ili Dobra vecer. Unos time varijable neka bude preko konzole, te ako korisnik nije unio ni jednu od mogucnosti neka ispis bude: Koje je to doba dana? Program napisi uz pomoc if-a i switch case-a.
+Napisi program koji ovisno o varijabli time mogucih vrijednosti ZORA, JUTRO, POSLIJEPODNE, POPODNE, PRIJEPODNE, PREDVECER, NOC 
+ispisuje Dobro jutro, Dobar dan ili Dobra vecer. Unos time varijable neka bude preko konzole, te ako korisnik nije unio ni 
+jednu od mogucnosti neka ispis bude: Koje je to doba dana? Program napisi uz pomoc if-a i switch case-a.
 */
 
-// tu san originalno stavia time.toLowerCase() pa usporediva s 'time' vrijednostima u malim slovima, cisto da bude svejedno upise li se npr. zora ili ZORA, ali onda kad se ne unese nista dobije se error umisto zadnji console.log
+// tu san originalno stavia time.toLowerCase() pa usporediva s 'time' vrijednostima u malim slovima, cisto da bude svejedno 
+// upise li se npr. zora ili ZORA, ali onda kad se ne unese nista dobije se error umisto zadnji console.log
 
 const time = process.argv.slice(2)[0];
 
@@ -47,13 +50,16 @@ switch (time) {
 
 /*
 ZADATAK 2
-Napisi program koji omogucava unos proizvoljnog broja godina preko konzole, te napisi ternarni izraz koji bira vrijednosti MALOLJETAN i PUNOLJETAN. Potom za MALOLJETAN ispisi "Korisnik je maloljentan", a za punoljetan "Korisnik je punoljetan". Ako korisnik nije unio broj napisi "Pogresan unos!".
+Napisi program koji omogucava unos proizvoljnog broja godina preko konzole, te napisi ternarni izraz koji bira 
+vrijednosti MALOLJETAN i PUNOLJETAN. Potom za MALOLJETAN ispisi "Korisnik je maloljentan", a za punoljetan 
+"Korisnik je punoljetan". Ako korisnik nije unio broj napisi "Pogresan unos!".
 */
 
 
 const age = +process.argv.slice(2)[0];
 
-// ovo san ovako s if jer kad bi iznad stavia || 'Pogresan unos' to bi mi se ispisalo kad nebi stavia broj, ali bi se i nastavia dalje izvrsavat ostatak koda (ternary i console.log)
+// ovo san ovako s if jer kad bi iznad stavia || 'Pogresan unos' to bi mi se ispisalo kad nebi stavia broj, 
+// ali bi se i nastavia dalje izvrsavat ostatak koda (ternary i console.log)
 
 if (!isNaN(age)) {
   const ternary = (age >= 18 ? 'PUNOLJETAN' : 'MALOLJETAN').toLowerCase();
@@ -71,7 +77,9 @@ if (!isNaN(age)) {
 
 /*
 ZADATAK 3
-Napisi program u kojem korisnik unosi broj preko konzole. Ako korisnik nije unio broj, program racuna da je korisnik unio broj 7. Program zatim ispisuje "Tvoj sretni broj je ___" i potom preko Math.random funkcije izvlaci cijeli broj od 0 do 9. Ukoliko je izvuceni broj identican korisnikovom sretnom broju ispisi "Jackpot!", a u suprotnom "Vise srece drugi put!".
+Napisi program u kojem korisnik unosi broj preko konzole. Ako korisnik nije unio broj, program racuna da je korisnik 
+unio broj 7. Program zatim ispisuje "Tvoj sretni broj je ___" i potom preko Math.random funkcije izvlaci cijeli broj 
+od 0 do 9. Ukoliko je izvuceni broj identican korisnikovom sretnom broju ispisi "Jackpot!", a u suprotnom "Vise srece drugi put!".
 Napomena: Ukoliko ne znate koristiti Math.random pokusajte guglati kako se koristi.
 */
 
